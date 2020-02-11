4ebfa776757ece3dc89dc56f948efa011e0ba4f8
Artur <grandfaraon93@gmail.com>
2020-02-11 16:07:54 +0200

    edited script for register SW

--- resources/views/offline.blade.php
+++ resources/views/offline.blade.php
@@ -177,12 +177,14 @@
 
         <script>
             if ('serviceWorker' in navigator) {
-                window.addEventListener('load', () => {
+                window.addEventListener("load", function() {
                     navigator.serviceWorker.register("{{ asset('service-worker.js') }}", {
                         scope: '.'
-                    })
-                    .then(registration => console.log('ServiceWorker registration successful with scope: ', registration.scope))
-                    .catch(err => console.log('ServiceWorker registration failed: ', err));
+                    }).then(function (registration) {
+                        console.log('ServiceWorker registration successful with scope: ', registration.scope);
+                    }, function (err) {
+                        console.log('ServiceWorker registration failed: ', err);
+                    });
                 });
             }
         </script>
