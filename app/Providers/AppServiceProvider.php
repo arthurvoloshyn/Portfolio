facc03c8ac73f1f72d5d02eb5dc4b55f479408cf
Arthur Voloshyn <grandfaraon93@gmail.com>
2020-02-29 11:36:15 +0200

    Updated boot

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -13,7 +13,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        if(env('APP_ENV') !== 'localhost')
+        {
+            URL::forceScheme('https');
+        }
     }
 
     /**
--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -30,8 +30,7 @@
     {{--ICONS--}}
 
     <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('favicons/apple-touch-icon.png') }}">
-    <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('favicons/favicon-32x32.png',
-    true) }}">
+    <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('favicons/favicon-32x32.png') }}">
     <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('favicons/favicon-16x16.png') }}">
     <link rel="icon" type="image/png" sizes="512x512" href="{{ asset('favicons/icons/icon-512x512.png') }}">
     <link rel="mask-icon" href="{{ asset('favicons/safari-pinned-tab.svg') }}" color="#65afc4">
