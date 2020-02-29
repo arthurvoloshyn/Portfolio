e33ed93cd80f9a7e8c91731cbeb2620067d212ab
Arthur Voloshyn <grandfaraon93@gmail.com>
2020-02-29 11:01:03 +0200

    Updated url

--- config/app.php
+++ config/app.php
@@ -52,9 +52,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://localhost'),
-
-    'asset_url' => env('ASSET_URL', null),
+    'url' => env('APP_URL', 'https://localhost'),
 
     /*
     |--------------------------------------------------------------------------
