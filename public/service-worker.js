13b671a193cd09087fb38ee376c3ed35e10f8847
Arthur Voloshyn <grandfaraon93@gmail.com>
2020-04-27 19:37:27 +0300

    Add logo for cache

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,5 +1,5 @@
 const staticCacheName = `static-cache-v${new Date().getTime()}`;
-const filesToCache = ['/offline', '/css/NotFound.css', '/js/libraries/NotFound.js', '/favicons/panda.png'];
+const filesToCache = ['/offline', '/css/NotFound.css', '/js/libraries/NotFound.js', '/favicons/panda.png', '/imgs/logo.png'];
 
 // Cache on install
 self.addEventListener('install', event => {
