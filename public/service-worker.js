6f8643fc651b049d449f0bc489e585aa54bf59e8
Arthur Voloshyn <grandfaraon93@gmail.com>
2020-04-28 15:54:31 +0300

    Edit offline page

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,34 +1,37 @@
-const staticCacheName = `static-cache-v${new Date().getTime()}`;
-const filesToCache = ['/offline', '/css/NotFound.css', '/js/libraries/NotFound.js', '/favicons/panda.png'];
+(() => {
+  const staticCacheName = `static-cache-v${new Date().getTime()}`;
+  const filesToCache = ['/offline', '/css/Offline.css', '/css/NotFound.css', '/js/libraries/NotFound.js', '/favicons/panda.png'];
 
-// Cache on install
-self.addEventListener('install', event => {
-  this.skipWaiting();
-  event.waitUntil(caches.open(staticCacheName).then(cache => cache.addAll(filesToCache)));
-});
+  // Cache on install
+  self.addEventListener('install', event => {
+    this.skipWaiting();
 
-// Clear cache on activate
-self.addEventListener('activate', event => {
-  event.waitUntil(
-    caches.keys().then(cacheNames =>
-      Promise.all(
-        cacheNames
-          .filter(cacheName => cacheName.startsWith('static-cache-'))
-          .filter(cacheName => cacheName !== staticCacheName)
-          .map(cacheName => caches.delete(cacheName))
+    event.waitUntil(caches.open(staticCacheName).then(cache => cache.addAll(filesToCache)));
+  });
+
+  // Clear cache on activate
+  self.addEventListener('activate', event => {
+    event.waitUntil(
+      caches.keys().then(cacheNames =>
+        Promise.all(
+          cacheNames
+            .filter(cacheName => cacheName.startsWith('static-cache-'))
+            .filter(cacheName => cacheName !== staticCacheName)
+            .map(cacheName => caches.delete(cacheName))
+        )
       )
-    )
-  );
+    );
 
-  this.clients.claim();
-});
+    this.clients.claim();
+  });
 
-// Serve from Cache
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches
-      .match(event.request)
-      .then(response => response || fetch(event.request))
-      .catch(() => caches.match('offline'))
-  );
-});
+  // Serve from Cache
+  self.addEventListener('fetch', event => {
+    event.respondWith(
+      caches
+        .match(event.request)
+        .then(response => response || fetch(event.request))
+        .catch(() => caches.match('offline'))
+    );
+  });
+})();
--- resources/views/offline.blade.php
+++ resources/views/offline.blade.php
@@ -1,194 +1,56 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 
-    <head>
-      <meta charset="utf-8">
-      <meta http-equiv="X-UA-Compatible" content="IE=edge">
-      <meta name="viewport" content="minimal-ui, width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
-      <meta name="theme-color" content="#ffffff" />
+  <head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="minimal-ui, width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" />
+    <meta name="theme-color" content="#ffffff" />
 
-      <title>{{ $meta['title'] }}</title>
+    <title>{{ $meta['title'] }}</title>
 
-      <link rel="manifest" href="{{ asset('favicons/manifest.json') }}">
+    <link rel="manifest" href="{{ asset('favicons/manifest.json') }}" />
+    <link rel="stylesheet" href="{{ asset('css/Offline.css') }}" />
+  </head>
 
-      <style>
-          * {
-              box-sizing: border-box;
-          }
+  <body>
 
-          html,
-          body {
-              padding: 0;
-              margin: 0;
-              height: 100%;
-              width: 100%;
-              font-family: 'Helvetica', 'Verdana', sans-serif;
-              font-weight: 400;
-              color: #444;
-              -webkit-font-smoothing: antialiased;
-              -moz-osx-font-smoothing: grayscale;
-          }
+    <noscript>You need to enable JavaScript to run this app.</noscript>
 
-          html {
-              overflow: hidden;
-          }
+    <header class="header">
+      <h1>
+        <a href="{{ url('/') }}">
+          ARTUR VOLOSHYN
+        </a>
+      </h1>
+    </header>
 
-          body {
-              display: -webkit-box;
-              display: -webkit-flex;
-              display: -ms-flexbox;
-              display: flex;
-              -webkit-box-orient: vertical;
-              -webkit-box-direction: normal;
-              -webkit-flex-direction: column;
-              -ms-flex-direction: column;
-              flex-direction: column;
-              -webkit-flex-wrap: nowrap;
-              -ms-flex-wrap: nowrap;
-              flex-wrap: nowrap;
-              -webkit-box-pack: start;
-              -webkit-justify-content: flex-start;
-              -ms-flex-pack: start;
-              justify-content: flex-start;
-              -webkit-box-align: stretch;
-              -webkit-align-items: stretch;
-              -ms-flex-align: stretch;
-              align-items: stretch;
-              -webkit-align-content: stretch;
-              -ms-flex-line-pack: stretch;
-              align-content: stretch;
-              background: #ececec;
-          }
+    <main class="main">
 
-          .header {
-              width: 100%;
-              height: 56px;
-              color: #FFF;
-              background: #3F51B5;
-              position: fixed;
-              font-size: 20px;
-              box-shadow: 0 4px 5px 0 rgba(0, 0, 0, 0.14), 0 2px 9px 1px rgba(0, 0, 0, 0.12), 0 4px 2px -2px rgba(0, 0, 0, 0.2);
-              padding: 16px 16px 0 16px;
-              will-change: transform;
-              display: -webkit-box;
-              display: -webkit-flex;
-              display: -ms-flexbox;
-              display: flex;
-              -webkit-box-orient: horizontal;
-              -webkit-box-direction: normal;
-              -webkit-flex-direction: row;
-              -ms-flex-direction: row;
-              flex-direction: row;
-              -webkit-flex-wrap: nowrap;
-              -ms-flex-wrap: nowrap;
-              flex-wrap: nowrap;
-              -webkit-box-pack: start;
-              -webkit-justify-content: flex-start;
-              -ms-flex-pack: start;
-              justify-content: flex-start;
-              -webkit-box-align: stretch;
-              -webkit-align-items: stretch;
-              -ms-flex-align: stretch;
-              align-items: stretch;
-              -webkit-align-content: center;
-              -ms-flex-line-pack: center;
-              align-content: center;
-              -webkit-transition: -webkit-transform 0.233s cubic-bezier(0, 0, 0.21, 1) 0.1s;
-              transition: -webkit-transform 0.233s cubic-bezier(0, 0, 0.21, 1) 0.1s;
-              transition: transform 0.233s cubic-bezier(0, 0, 0.21, 1) 0.1s;
-              z-index: 1000;
-          }
+      <div class="offline-card">
+        <img src="{{ asset('favicons/panda.png') }}" class="offline-panda" alt="panda" />
+        <div class="description">
+          Oops, you appear to be offline, this app requires an internet connection.
+        </div>
+      </div>
 
-          .header h1 {
-              font-weight: 400;
-              font-size: 20px;
-              margin: 0;
-              -webkit-box-flex: 1;
-              -webkit-flex: 1;
-              -ms-flex: 1;
-              flex: 1;
-          }
+    </main>
 
-          .header h1 a {
-              text-decoration: none;
-              color: #FFF;
-          }
+    <script>
+      if ('serviceWorker' in navigator) {
+        window.addEventListener("load", () => {
+          navigator.serviceWorker.register("{{ asset('service-worker.js') }}", {
+            scope: '.'
+          }).then(registration => {
+            console.log('ServiceWorker registration successful with scope: ', registration.scope);
+          }).catch(err => {
+            console.log('ServiceWorker registration failed: ', err);
+          });
+        });
+      }
 
-          .main {
-              padding-top: 60px;
-              -webkit-box-flex: 1;
-              -webkit-flex: 1;
-              -ms-flex: 1;
-              flex: 1;
-              overflow-x: hidden;
-              overflow-y: auto;
-              -webkit-overflow-scrolling: touch;
-          }
+    </script>
 
-          .offline-card {
-              padding: 16px;
-              position: relative;
-              box-sizing: border-box;
-              background: #fff;
-              border-radius: 2px;
-              margin: 16px;
-              box-shadow: 0 2px 2px 0 rgba(0, 0, 0, 0.14), 0 3px 1px -2px rgba(0, 0, 0, 0.2), 0 1px 5px 0 rgba(0, 0, 0, 0.12);
-          }
-
-          .offline-panda {
-              margin-left: auto;
-              margin-right: auto;
-              margin-bottom: 1.5em;
-              display: block;
-          }
-
-          .description {
-              text-align: center;
-          }
-
-      </style>
-    </head>
-
-    <body>
-
-        <noscript>You need to enable JavaScript to run this app.</noscript>
-
-        <header class="header">
-            <h1>
-                <a href="{{ url('/') }}">
-                    ARTUR VOLOSHYN
-                </a>
-            </h1>
-        </header>
-
-        <main class="main">
-
-            <div class="offline-card">
-                <img
-                    src="{{ asset('favicons/panda.png') }}"
-                    class="offline-panda" alt="panda">
-                <div class="description">
-                    Oops, you appear to be offline, this app requires an internet
-                    connection.
-                </div>
-            </div>
-
-        </main>
-
-        <script>
-            if ('serviceWorker' in navigator) {
-                window.addEventListener("load", function() {
-                    navigator.serviceWorker.register("{{ asset('service-worker.js') }}", {
-                        scope: '.'
-                    }).then(function (registration) {
-                        console.log('ServiceWorker registration successful with scope: ', registration.scope);
-                    }, function (err) {
-                        console.log('ServiceWorker registration failed: ', err);
-                    });
-                });
-            }
-        </script>
-
-    </body>
+  </body>
 
 </html>
