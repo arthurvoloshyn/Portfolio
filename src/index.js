a6d15d8530ca273ea5978256009dfaf523066cfe
arturvoloshyn <arturvoloshyn@gmail.com>
2022-12-31 06:28:06 +0200

    Replace BrowserRouter with HashRouter

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { HashRouter as Router } from 'react-router-dom';
 
 import * as serviceWorker from './serviceWorker';
 import store from './store';
