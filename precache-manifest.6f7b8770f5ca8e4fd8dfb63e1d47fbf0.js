77843c1c56cb33cd914aa9c6a647c701cf51c477
arturvoloshyn <arturvoloshyn@gmail.com>
2022-12-31 06:24:13 +0200

    Updates

---
+++ precache-manifest.6f7b8770f5ca8e4fd8dfb63e1d47fbf0.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "424cb3686db288da16dbf3e5e328b62b",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "80de7a4b9528df76b145",
+    "url": "/Portfolio/static/css/2.2d3d9491.chunk.css"
+  },
+  {
+    "revision": "5985850dc4c4504a3a5f",
+    "url": "/Portfolio/static/css/3.8fc694a2.chunk.css"
+  },
+  {
+    "revision": "925765d872c41d9dd51e",
+    "url": "/Portfolio/static/css/4.c48313b1.chunk.css"
+  },
+  {
+    "revision": "b5cc5c57e872b8070a4d",
+    "url": "/Portfolio/static/css/5.35b0f4b9.chunk.css"
+  },
+  {
+    "revision": "077820b4de707d77b9a0",
+    "url": "/Portfolio/static/css/6.2e429b7c.chunk.css"
+  },
+  {
+    "revision": "74df8b59a743dc2cc149",
+    "url": "/Portfolio/static/css/8.fad0d58b.chunk.css"
+  },
+  {
+    "revision": "4508c0b4c74bc5a0d52f",
+    "url": "/Portfolio/static/css/main.19a177e4.chunk.css"
+  },
+  {
+    "revision": "80de7a4b9528df76b145",
+    "url": "/Portfolio/static/js/2.9038f105.chunk.js"
+  },
+  {
+    "revision": "5985850dc4c4504a3a5f",
+    "url": "/Portfolio/static/js/3.6060916e.chunk.js"
+  },
+  {
+    "revision": "925765d872c41d9dd51e",
+    "url": "/Portfolio/static/js/4.23ebdb17.chunk.js"
+  },
+  {
+    "revision": "b5cc5c57e872b8070a4d",
+    "url": "/Portfolio/static/js/5.8e4f8183.chunk.js"
+  },
+  {
+    "revision": "077820b4de707d77b9a0",
+    "url": "/Portfolio/static/js/6.68e87ee8.chunk.js"
+  },
+  {
+    "revision": "a4cf38d4ee4925e459fc",
+    "url": "/Portfolio/static/js/7.e34aeefc.chunk.js"
+  },
+  {
+    "revision": "74df8b59a743dc2cc149",
+    "url": "/Portfolio/static/js/8.3437f5bf.chunk.js"
+  },
+  {
+    "revision": "812aee3d7391df3c08db",
+    "url": "/Portfolio/static/js/9.edb97eb4.chunk.js"
+  },
+  {
+    "revision": "4508c0b4c74bc5a0d52f",
+    "url": "/Portfolio/static/js/main.9dbe5c3b.chunk.js"
+  },
+  {
+    "revision": "d761aac718138958e9e9",
+    "url": "/Portfolio/static/js/runtime~main.7ee7b6f7.js"
+  }
+]);
